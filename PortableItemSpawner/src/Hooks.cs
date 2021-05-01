7e0a65b4b8328a9c0d80b6d20c9e5423b95c674e
M'aiq <m.janik1031@gmail.com>
2021-04-30 21:11:47 -0500

    More cleanup

--- PortableItemSpawner/src/Hooks.cs
+++ PortableItemSpawner/src/Hooks.cs
@@ -1,4 +1,5 @@
 ﻿using FistVR;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,7 +17,7 @@ namespace PortableItemSpawner
 			"LockButton",
 			"Cube",
 		};
-
+		private GameObject _optionsPanelPrefab;
 		private FVRPhysicalObject _portableItemSpawner;
 		public FVRPhysicalObject PortableItemSpawner
 		{
@@ -27,12 +28,13 @@ namespace PortableItemSpawner
 					return _portableItemSpawner;
 				}
 
-				// Get the wristmenu
-				var bod = GM.CurrentPlayerBody;
-				var wristMenu = bod.RightHand.GetComponent<FVRViveHand>().m_wristmenu ? null : bod.LeftHand.GetComponent<FVRViveHand>().m_wristmenu;
+				if (_optionsPanelPrefab is null)
+				{
+					throw new InvalidOperationException("The donor options panel prefab is null!");
+				}
 
 				// Get prefab from wristmenu
-				var panelObject = GameObject.Instantiate(wristMenu.OptionsPanelPrefab, Vector3.zero, Quaternion.identity);
+				var panelObject = GameObject.Instantiate(_optionsPanelPrefab, Vector3.zero, Quaternion.identity);
 				panelObject.name = "PortableItemSpawner";
 
 				// Delete all unneeded components & children
@@ -41,7 +43,7 @@ namespace PortableItemSpawner
 				DeleteAllBut(panelTransform, _rootKeep);
 
 				// Fix button placement
-				var btnTransform = panelTransform.Find("LockButton").transform;
+				var btnTransform = panelTransform.Find("LockButton");
 				btnTransform.localPosition = new Vector3(0.2185f, 0.1135f, -0.12f);
 				btnTransform.rotation = Quaternion.Euler(270, 0, 180);
 
@@ -71,16 +73,30 @@ namespace PortableItemSpawner
 
 		private void Hook()
 		{
-			On.FistVR.ItemSpawnerUI.Start += ItemSpawnerUI_Start;
+			// Get the options panel prefab
+			On.FistVR.FVRWristMenu.Awake += (orig, self) => {
+				orig(self);
+				_optionsPanelPrefab = self.OptionsPanelPrefab;
+			};
+
+			// Get the ItemSpawner gameobject
+			On.FistVR.ItemSpawnerUI.Start += (orig, self) => {
+				orig(self);
+				_itemSpawner = self.transform.parent.parent.gameObject;
+			};
+
+			// Enable itemspawner on death to vault match gun
+			On.FistVR.TNH_Manager.PlayerDied += (orig, self) => {
+				orig(self);
+				self.ItemSpawner.SetActive(true);
+			};
+
 			On.FistVR.FVRWristMenu.SpawnOptionsPanel += FVRWristMenu_SpawnOptionsPanel;
-			On.FistVR.TNH_Manager.PlayerDied += TNH_Manager_PlayerDied;
 		}
 
 		private void Unhook()
 		{
-			On.FistVR.ItemSpawnerUI.Start -= ItemSpawnerUI_Start;
 			On.FistVR.FVRWristMenu.SpawnOptionsPanel -= FVRWristMenu_SpawnOptionsPanel;
-			On.FistVR.TNH_Manager.PlayerDied -= TNH_Manager_PlayerDied;
 		}
 
 		// Spawn our panel whenever options panel is clicked
@@ -91,29 +107,12 @@ namespace PortableItemSpawner
 			if (_itemSpawner != null)
 			{
 				var maxDist = 2;
-				if (_portableItemSpawner == null || Vector3.Distance(GM.CurrentPlayerBody.Head.position, PortableItemSpawner.transform.position) > maxDist)
+				if (PortableItemSpawner == null || Vector3.Distance(GM.CurrentPlayerBody.Head.position, PortableItemSpawner.transform.position) > maxDist)
 				{
 					PortableItemSpawner.transform.position = self.m_currentHand.transform.position + GM.CurrentPlayerBody.Head.transform.forward;
 					PortableItemSpawner.transform.rotation = Quaternion.Euler(270, 0, 0);
 				}
 			}
-		}
-
-		// Get the ItemSpawner gameobject
-		private void ItemSpawnerUI_Start(On.FistVR.ItemSpawnerUI.orig_Start orig, ItemSpawnerUI self)
-		{
-			orig(self);
-
-			// Get ItemSpawner from the Canvas
-			_itemSpawner = self.transform.parent.parent.gameObject;
-		}
-
-		// Enable itemspawner on death to vault match gun
-		private void TNH_Manager_PlayerDied(On.FistVR.TNH_Manager.orig_PlayerDied orig, TNH_Manager self)
-		{
-			orig(self);
-
-			self.ItemSpawner.SetActive(true);
 		}
 
 		private void DeleteAllBut(Transform parent, HashSet<string> exceptions)
